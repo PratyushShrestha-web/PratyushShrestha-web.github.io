580e887082211a7e587b848230dbfe9c29cf05e8
Pratyush Shrestha <121381876+PratyushShrestha-web@users.noreply.github.com>
2026-07-04 16:22:36 +0545

    Add responsive breakpoints for better mobile support

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ export default {
       fontFamily: {
         kanit: ['Kanit', 'sans-serif'],
       },
+      screens: {
+        'xs': '320px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
     },
   },
   plugins: [],
